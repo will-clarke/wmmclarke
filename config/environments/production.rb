3c9a71ff5f577ec18615b75c17231cba3816014f
wmmc <wmmclarke@gmail.com>
2014-09-03 01:06:07 +0100

    Added Tag Manager & Analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ config.action_mailer.delivery_method = :smtp
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'example.com',
+  domain:               'wmmclarke.com',
   user_name:            ENV['GMAIL_EMAIL'],
   password:             ENV['GMAIL_PASSWORD'],
   authentication:       'plain',
