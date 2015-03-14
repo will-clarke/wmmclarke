97f176892a1cc220ecdfc6790f6e9452777fd627
William <wmmclarke@gmail.com>
2015-03-14 21:53:09 +0000

    Static assets for blog

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,14 +1,16 @@
 Rails.application.configure do
 
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'wmmclarke.com',
-  user_name:            ENV['GMAIL_EMAIL'],
-  password:             ENV['GMAIL_PASSWORD'],
-  authentication:       'plain',
-  enable_starttls_auto: true  }
+  config.serve_static_files = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'wmmclarke.com',
+    user_name:            ENV['GMAIL_EMAIL'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Settings specified here will take precedence over those in config/application.rb.
 
